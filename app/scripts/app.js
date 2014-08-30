1d9e4e6fdb41216c34ebc4fc4c06f012d105c0d0
Jesus Parlange <jesus.parlange@gmail.com>
2014-08-30 14:54:36 -0700

    empty template

---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('newJApp', [])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
