792d02fa69d4589ae84f6e7da8293e378beefc99
Jesus Parlange <jesus.parlange@gmail.com>
2014-09-08 00:04:21 -0700

    work samples added

--- app/scripts/directives.js
+++ app/scripts/directives.js
@@ -4,7 +4,7 @@ app.directive('elements', function($timeout){
 	return{
 		restrict: 'A',
 		scope: false,
-		link: function(scope, element, attrs){
+		link: function(scope, element){
 
 			var text = element.find('.aboutBox');
 			var image = element.find('.travel-img');
@@ -26,4 +26,63 @@ app.directive('elements', function($timeout){
 			});
 		}
 	};
+});
+
+app.directive('wrapOwlcarousel', function () {  
+    return {  
+        restrict: 'E',  
+        link: function (scope, element, attrs) {  
+            var options = scope.$eval($(element).attr('data-options'));  
+            $(element).owlCarousel(options);  
+        }  
+    };  
+});  
+
+app.directive('latestWorks', function(){
+	return{
+		restrict: 'A',
+		link: function(scope, element){
+			var colours = [
+				'#6c88b9','#72d2a5','#f88890', '#f49662'
+			
+			];
+
+			var boxColor = element.find('.latest-box');
+			var number = 0;
+
+			element.on('mouseover', function(){
+				element.css('cursor', 'pointer');
+			});
+
+			element.on('click', function(){
+				console.log('click');
+
+				boxColor.css('background-color', colours[number]);
+
+				number += 1;
+
+				if(number > 3){
+						number = 0;
+				}
+				
+
+				console.log(number);
+
+			});
+
+		}
+	}
+});	
+
+app.directive('jesusParlange', function($timeout, $interval){
+	return{
+		restrict: 'A',
+		link: function(scope, element){
+
+			$interval(function(){
+				element.toggleClass('colour-photo');
+			}, 9000);
+
+		}
+	}
 });
